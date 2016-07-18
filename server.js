7e4ee5daa30d5cbf49b3596520dbbbfff57df61f
Mishko <mishko.meteor@gmail.com>
2016-07-18 10:22:49 +0300

    #01 initial commit

---
+++ server.js
@@ -0,0 +1,13 @@
+var express = require('express');
+
+var app = express();
+
+app.use(express.static(__dirname + '/'));
+
+app.get('/', function (req, res) { 
+    res.sendFile('index.html');
+});
+
+app.listen(3001, function () {
+    console.log('got a hit!')
+});
